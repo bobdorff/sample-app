633ab6232b7a5de69ee31d7a14760ab6d53b9a6b
bobdorff <robertdorff@gmail.com>
2016-04-27 12:45:29 -0500

    complete user model tests/validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  EMAIL_VALIDATION = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates(:name,  presence: true, length: {maximum: 50})
-  validates(:email, presence: true, length: {maximum: 200})
+  validates(:email, presence: true, length: {maximum: 200}, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}, uniqueness: {case_sensitive: false})
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -28,6 +28,30 @@ class UserTest < ActiveSupport::TestCase
     @user.email = "b" * 201
     assert_not @user.valid?
   end
+
+  test "email validation should accept valid address" do 
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |address|
+      @user.email = address
+      assert @user.valid?, "#{address.inspect} should be valid"                     
+    end
+  end
+
+  test "email validation rejects invalid addresses" do 
+    invalid_addresses = %w[user.example.com User user@foo user@email.com. foo@bar+baz.com]
+    invalid_addresses.each do |address|
+      @user.email = address
+      assert_not @user.valid?, "#{address.inspect} should be invalid"
+    end
+  end
+
+  test "email validation rejects non-unique emails" do 
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
 end
 
 
