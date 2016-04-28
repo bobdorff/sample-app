a180e8a6a13f6671561f9c76472b03997fc13135
bobdorff <robertdorff@gmail.com>
2016-04-28 08:46:50 -0500

    tests for user login

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,10 @@ class User < ActiveRecord::Base
   validates(:name,  presence: true, length: {maximum: 50})
   validates(:email, presence: true, length: {maximum: 200}, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}, uniqueness: {case_sensitive: false})
   validates(:password, :password_confirmation, presence: true, length: {minimum: 6})
+
+  #creates a hash digest of a string
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
