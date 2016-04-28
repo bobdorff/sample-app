c088f9db91c2ebf62d90b67184a12756228348fd
bobdorff <robertdorff@gmail.com>
2016-04-28 07:43:41 -0500

    write login error handler and integration test for login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(emal: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # log em in
+    else
+      flash[:danger] = 'Incorrect email or password. Please try again.'
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
 end
