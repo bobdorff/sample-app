bd32e8cf4a2a881c40a03ef7a609f3d286579e66
bobdorff <robertdorff@gmail.com>
2016-04-27 20:16:45 -0500

    complete integration tests for signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -9,7 +9,20 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                               password_confirmation: "bar"
                               }
     end
-      assert_template 'users/new'
-      assert_select "div#error_explination"
+    assert_template 'users/new'
+    assert_select "div#error_explination"
+  end
+
+  test 'it accepts a valid signup and redirects correctly' do
+    get signup_path
+    assert_difference "User.count", 1 do 
+      post_via_redirect users_path, user: {name: "Bob", 
+                              email: "user@valid.com",
+                              password: "foooooo",
+                              password_confirmation: "foooooo"
+                              }
+    end
+    assert_template 'users/show'
+    assert_select "div.alert"
   end
 end
